c99fca535d0716992eaf4ae73d965563b0b27193
Yogurt_with_thoughts <skrytnyjezik@gmail.com>
2024-11-29 14:19:11 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Core/Navigation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MPAccses.MVVM.Core
+{
+    internal class Navigation
+    {
+      
+            public static class CoreNavigate
+            {
+                public static Frame NavigatorCore { get; set; }
+            }
+        
+    }
+}
---
+++ MVVM/View/Pages/MainScreen.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MPAccses.MVVM.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainScreen.xaml
+    /// </summary>
+    public partial class MainScreen : Page
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameTextBox.Text))
+            {
+                WatermarkText.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                WatermarkText.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+    
+}
